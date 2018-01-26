6e67ed1057a4d71f1afe3638555b0f801193bd73
adriangohjw <adriangohjw@gmail.com>
2018-01-26 13:57:20 +0800

    spelling errors and changing from float to double

--- lab01/lab01_prac_04/main.c
+++ lab01/lab01_prac_04/main.c
@@ -5,13 +5,13 @@
 
 int main()
 {
-    float x1, y1, x2, y2;
+    double x1, y1, x2, y2;
     printf("Enter first point x1 y1:\n");
-    scanf("\n%f %f", &x1, &y1);
-    printf("Enter first point x2 y2:\n");
-    scanf("\n%f %f", &x2, &y2);
+    scanf("%lf %lf", &x1, &y1);
+    printf("Enter second point x2 y2:\n");
+    scanf("%lf %lf", &x2, &y2);
 
-    float distance = sqrt((pow(x1 - x2, 2)) + pow(y1 - y2, 2));
-    printf("The distance is %.2f", distance);
+    double distance = sqrt((pow(x1 - x2, 2)) + pow(y1 - y2, 2));
+    printf("The distance is %.2lf", distance);
     return 0;
 }
