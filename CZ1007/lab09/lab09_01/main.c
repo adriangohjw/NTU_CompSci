d465fe0c768170df0a8cd688ab139f71b82335e9
adriangohjw <adriangohjw@gmail.com>
2018-03-19 15:00:53 +0800

    + lab09 question 1 added

---
+++ CZ1007/lab09/lab09_01/main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int value;
+    struct node *next;
+} ListNode;
+
+void printList(ListNode *head);
+int removeNode(ListNode **ptrHead, int index);
+ListNode * findNode(ListNode *head, int index);
+
+int main()
+{
+    ListNode *head = NULL, *temp;
+
+    int num;
+    printf("Enter a list of numbers, terminated by the value -1:\n");
+    scanf("%d", &num);
+
+    while (num != -1){
+        if (head == NULL){
+            // if Linked List currently empty
+            head = malloc(sizeof(ListNode));
+            temp = head;
+        } else {
+            // if Linked List is not empty
+            temp->next = malloc(sizeof(ListNode));
+            temp = temp->next;
+        }
+        // temp is the last node
+        temp->value = num;
+        temp->next = NULL;
+
+        scanf("%d", &num);
+    }
+
+    printf("Current list: ");
+    printList(head);
+
+    int askRemove;
+    printf("Node at which index to remove? (-1 to Exit): ");
+    scanf("%d", &askRemove);
+    while (askRemove != -1){
+        switch(removeNode(&head, askRemove)){
+        case 0:  // successfully removed
+            printf("Successfully removed!\n");
+            break;
+        case -1:  // unsuccessful removal
+            printf("Removal failed!\n");
+            break;
+        default:
+            printf("Error!\n");
+            break;
+        }
+        printList(head);
+        printf("Node at which index to remove? (-1 to Exit): ");
+        scanf("%d", &askRemove);
+    }
+
+    return 0;
+}
+
+void printList(ListNode *head){
+    ListNode *temp = head;
+    while (temp != NULL){
+        printf("%d ", temp->value);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+ListNode * findNode(ListNode *head, int index){
+    ListNode *temp = head;
+    if (head == NULL || index < 0)
+        return NULL;
+    while (index > 0){
+        temp = temp->next;
+        if (temp == NULL)
+            return NULL;
+        index--;
+    }
+    return temp;
+}
+
+int removeNode(ListNode **ptrHead, int index){
+    // **ptrHead is the address of the first node
+    //  *ptrHead is the first node
+    if (index < 0)  // error if negative index chosen
+        return -1;
+    else if (index == 0){  // if first node chosen to be removed
+        if ((*ptrHead)->next == NULL)  // first node is only node
+            (*ptrHead) = NULL;
+        else
+            (*ptrHead) = (*ptrHead)->next;  // move address of first node to second node
+        return 0;
+    } else {
+        ListNode *pre = findNode(*ptrHead, index-1), *cur;
+        if (pre == NULL || pre->next == NULL)
+            return -1;  // current is the currentNode
+        else {
+            cur = pre->next->next;
+            pre->next = cur;
+            return 0;
+        }
+    }
+}
