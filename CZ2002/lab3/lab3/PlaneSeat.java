fa63f42e589caee77cf4f90ce5d7b1ff476af2d7
NodeFlair <adriangohjw@gmail.com>
2019-09-19 03:11:55 +0800

    added PlaneSeat

--- CZ2002/lab3/lab3/PlaneSeat.java
+++ CZ2002/lab3/lab3/PlaneSeat.java
@@ -2,4 +2,33 @@ package lab3;
 
 public class PlaneSeat {
 
+	private int seatId;
+	private boolean assigned;
+	private int customerId;
+	
+	public PlaneSeat(int seat_id) {
+		this.seatId = seat_id;
+	}
+	
+	public int getSeatID() {
+		return this.seatId;
+	}
+	
+	public int getCustomerID() {
+		return this.customerId;
+	}
+	
+	public boolean isOccupied() {
+		return this.assigned;
+	}
+	
+	public void assign(int cust_id) {
+		this.customerId = cust_id;
+		this.assigned = true;
+	}
+	
+	public void unAssign() {
+		this.customerId = -1;
+		this.assigned = false;
+	}
 }
