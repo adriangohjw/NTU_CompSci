4f080ef056310417bd3fae020f967d9184b2e71e
adrian <adriangohjw@gmail.com>
2019-10-29 06:08:27 +0800

    added lab3

---
+++ CZ2005/lab3/tlb.cc
@@ -0,0 +1,309 @@
+#include "copyright.h"
+#include "tlb.h"
+#include "syscall.h"
+#include "machine.h"
+#include "thread.h"
+#include "system.h"
+#include "utility.h"
+
+//----------------------------------------------------------------------
+// UpdateTLB
+//      Called when exception is raised and a page isn't in the TLB.
+// Figures out what to do (get from IPT, or pageoutpagein) and does it.
+//----------------------------------------------------------------------
+
+void UpdateTLB(int possible_badVAddr)
+{
+  int badVAddr;
+  unsigned int vpn;
+  int phyPage;
+
+  if(possible_badVAddr) // get the bad address from the correct location
+    badVAddr=possible_badVAddr; // fault in kernel
+  else
+    badVAddr=machine->registers[BadVAddrReg]; // fault in userprog
+  
+  vpn=(unsigned)badVAddr/PageSize;
+  
+  if((phyPage=VpnToPhyPage(vpn))!=-1) {
+    InsertToTLB(vpn, phyPage);
+  } else {
+    if(vpn>=currentThread->space->numPages && !GetMmap(vpn))
+      machine->RaiseException(AddressErrorException, badVAddr);
+    else
+      InsertToTLB(vpn, PageOutPageIn(vpn));
+  }
+}
+
+//----------------------------------------------------------------------
+// VpnToPhyPage
+//      Gets a phyPage for a vpn, if exists in ipt.
+//----------------------------------------------------------------------
+
+int VpnToPhyPage(int vpn)
+{
+  //your code here to get a physical frame for page vpn
+  //you can refer to PageOutPageIn(int vpn) to see how an entry was created in ipt
+
+  // DEBUG message to display all the values of a physical frame
+  for (int i = 0; i < NumPhysPages; i++)
+     DEBUG('p', "...IPT %i, pid, vpn, last used and valid is %i %i %i %i \n", i, memoryTable[i].pid, memoryTable[i].vPage, memoryTable[i].lastUsed, memoryTable[i].valid);
+
+  // Loop through to find the correct physical page by comparing pid and vpn
+  // If found, return the physical frame number
+  // Else, return -1
+  for (int i = 0; i < NumPhysPages; i++){
+	if ((memoryTable[i].valid) && (memoryTable[i].pid == currentThread->pid) && (memoryTable[i].vPage == vpn)){
+		DEBUG('p', ".....Found IPT %i, pid, vpn, last used and valid is %i %i %i %i \n", i, memoryTable[i].pid, memoryTable[i].vPage, memoryTable[i].lastUsed, memoryTable[i].valid);
+		return i;
+	}
+  }
+  DEBUG('p', ".....IPT with matching pid and vpn NOT found!\n");
+  return -1;
+}
+
+//----------------------------------------------------------------------
+// InsertToTLB
+//      Put a vpn/phyPage combination into the TLB. If TLB is full, use FIFO 
+//    replacement
+//----------------------------------------------------------------------
+
+
+void InsertToTLB(int vpn, int phyPage)
+{
+  int i = 0; //entry in the TLB
+  
+  //your code to find an empty in TLB or to replace the oldest entry if TLB is full
+
+  // value of FIFOPointer is the TLB that will be replaced next
+  static int FIFOPointer = 0;  
+
+  // Using a loop to find an empty entry in TLB
+  // First checks whether there is an old entry that is invalid (valid bit == 0)
+  // If invalid entry found, break and exit the loop
+  // Else, FIFOPointer to point to the oldest entry that will be removed next
+  bool isEmpty  = false;
+  for (i = 0; i < TLBSize; i++){
+	if (!machine->tlb[i].valid){
+		isEmpty  = true;
+		break;
+	}
+  }
+  if (!isEmpty){
+	i = FIFOPointer;
+  }
+
+  // if an entry is just inserted, then the entry next to it is the oldest entry
+  FIFOPointer = (i + 1) % TLBSize;
+ 
+  // copy dirty data to memoryTable
+  if(machine->tlb[i].valid){
+    memoryTable[machine->tlb[i].physicalPage].dirty=machine->tlb[i].dirty;
+    memoryTable[machine->tlb[i].physicalPage].TLBentry=-1;
+  }
+  // to show the TLB and their respective values
+  DEBUG('p', "Before updating TLB %i \n", i);
+  for (int j=0; j<3; j++)
+    DEBUG('p', "...TLB %i, vpn, phy and valid is %i %i %i \n", j, machine->tlb[j].virtualPage, machine->tlb[j].physicalPage, machine->tlb[j].valid);
+
+  //update the TLB entry
+  machine->tlb[i].virtualPage  = vpn;
+  machine->tlb[i].physicalPage = phyPage;
+  machine->tlb[i].valid        = TRUE;
+  machine->tlb[i].readOnly     = FALSE;
+  machine->tlb[i].use          = FALSE;
+  machine->tlb[i].dirty        = memoryTable[phyPage].dirty;
+
+  DEBUG('p', "After updating TLB %i \n", i);
+  DEBUG('p', "...TLB %i, vpn, phy and valid is %i %i %i \n", i, machine->tlb[i].virtualPage, machine->tlb[i].physicalPage, machine->tlb[i].valid);
+
+  //update the corresponding memoryTable
+  memoryTable[phyPage].TLBentry=i;
+  DEBUG('p', "The corresponding TLBentry for Page %i in TLB is %i ", vpn, i);
+  //reset lastUsed to current ticks since it is being used at this moment.
+  //for the implementation of LRU algorithm.
+  memoryTable[phyPage].lastUsed = stats->totalTicks; 
+  
+  //increase the number of tlb misses
+  stats->numTlbMisses++;
+  
+}
+
+//----------------------------------------------------------------------
+// PageOutPageIn
+//      Calls DoPageOut and DoPageIn and handles memoryTable
+// bookkeeping. Use lru algorithm to find the replacement page.
+//----------------------------------------------------------------------
+
+int PageOutPageIn(int vpn)
+{
+  int phyPage; 
+  
+  //increase the number of page faults
+  stats->numPageFaults++;
+  //call the LRU algorithm, which returns the freed physical frame
+  phyPage=lruAlgorithm();
+  
+  //Page out the victim page to free the physical frame
+  DoPageOut(phyPage);
+  //Page in the new page to the freed physical frame
+  DoPageIn(vpn, phyPage);
+  
+  //update memoryTable for this frame
+  memoryTable[phyPage].valid=TRUE;
+  memoryTable[phyPage].pid=currentThread->pid;
+  memoryTable[phyPage].vPage=vpn;
+  memoryTable[phyPage].dirty=FALSE;
+  memoryTable[phyPage].TLBentry=-1;
+  memoryTable[phyPage].lastUsed=0;
+  memoryTable[phyPage].swapPtr=currentThread->space->swapPtr;
+  
+  return phyPage;
+}
+
+//----------------------------------------------------------------------
+// DoPageOut
+//      Actually pages out a phyPage to it's swapfile.
+//----------------------------------------------------------------------
+
+void DoPageOut(int phyPage)
+{
+  MmapEntry *mmapPtr;
+  int numBytesWritten;
+  int mmapBytesToWrite;
+
+  if(memoryTable[phyPage].valid){           // check if pageOut possible
+    if(memoryTable[phyPage].TLBentry!=-1){
+      memoryTable[phyPage].dirty=
+        machine->tlb[memoryTable[phyPage].TLBentry].dirty;
+      machine->tlb[memoryTable[phyPage].TLBentry].valid=FALSE;
+    }
+    if(memoryTable[phyPage].dirty){        // pageOut is necessary
+      if((mmapPtr=GetMmap(memoryTable[phyPage].vPage))){ // it's mmaped
+        DEBUG('p', "mmap paging out: pid %i, phyPage %i, vpn %i\n",
+          memoryTable[phyPage].pid, phyPage, memoryTable[phyPage].vPage);
+        if(memoryTable[phyPage].vPage==mmapPtr->endPage)
+          mmapBytesToWrite=mmapPtr->lastPageLength;
+        else
+          mmapBytesToWrite=PageSize;
+        numBytesWritten=mmapPtr->openFile->
+          WriteAt(machine->mainMemory+phyPage*PageSize, mmapBytesToWrite,
+            (memoryTable[phyPage].vPage-mmapPtr->beginPage)*PageSize);
+        ASSERT(mmapBytesToWrite==numBytesWritten);
+      } else { // it's not mmaped
+        DEBUG('p', "paging out: pid %i, phyPage %i, vpn %i\n",
+          memoryTable[phyPage].pid, phyPage, memoryTable[phyPage].vPage);
+        numBytesWritten=memoryTable[phyPage].swapPtr->
+          WriteAt(machine->mainMemory+phyPage*PageSize, PageSize,
+            memoryTable[phyPage].vPage*PageSize);
+        ASSERT(PageSize==numBytesWritten);
+      }
+      
+      //increase the number of page faults
+      stats->numPageOuts++;
+    }
+    
+    memoryTable[phyPage].valid=FALSE;
+  }
+}
+
+//----------------------------------------------------------------------
+// DoPageIn
+//      Actually pages in a phyPage/vpn combo from the swapfile.
+//----------------------------------------------------------------------
+
+void DoPageIn(int vpn, int phyPage)
+{
+  MmapEntry *mmapPtr;
+  int numBytesRead;
+  int mmapBytesToRead;
+
+  if((mmapPtr=GetMmap(vpn))){ // mmaped file
+    DEBUG('p', "mmap paging in: pid %i, phyPage %i, vpn %i\n",
+      currentThread->pid, phyPage, vpn);
+    if(vpn==mmapPtr->endPage)
+      mmapBytesToRead=mmapPtr->lastPageLength;
+    else
+      mmapBytesToRead=PageSize;
+    numBytesRead=
+      mmapPtr->openFile->ReadAt(machine->mainMemory+phyPage*PageSize,
+                mmapBytesToRead,
+                (vpn-mmapPtr->beginPage)*PageSize);
+    ASSERT(numBytesRead==mmapBytesToRead);
+  } else { // not mmaped
+    DEBUG('p', "paging in: pid %i, phyPage %i, vpn %i\n", currentThread->pid,
+      phyPage, vpn);
+    numBytesRead=currentThread->space->swapPtr->ReadAt(machine->mainMemory+
+                   phyPage*PageSize,
+                   PageSize,
+                   vpn*PageSize);
+    ASSERT(PageSize==numBytesRead);
+  }
+}
+
+//----------------------------------------------------------------------
+// lruAlgorithm
+//      Determine where a vpn should go in phymem, and therefore what
+// should be paged out. This lru algorithm is the one discussed in the 
+// lectures.
+//----------------------------------------------------------------------
+
+int lruAlgorithm(void)
+{
+  //your code here to find the physical frame that should be freed 
+  //according to the LRU algorithm. 
+
+  // Firsty, it checks if there's an invalid frame. If so, make the invalid frame the victim page. 
+  // However, if all pages are valid, then LRU compares the last used information in each frame to select the victim page
+  int phyPage = 0;
+  int last = memoryTable[0].lastUsed;  // last used field
+  for (int i = 0; i < NumPhysPages; i++){
+	if (!memoryTable[i].valid){ // if free frame is found
+		phyPage = i;
+		break;
+	}
+	if (memoryTable[i].lastUsed < last){  // no free frame found
+		last = memoryTable[i].lastUsed;
+		phyPage = i;
+	}
+  }
+  
+  return phyPage;
+}
+
+//----------------------------------------------------------------------
+// GetMmap
+//      Return an MmapEntry structure corresponding to the vpn.  Returns
+// 0 if does not exist.
+//----------------------------------------------------------------------
+
+MmapEntry *GetMmap(int vpn)
+{
+  MmapEntry *mmapPtr;
+
+  mmapPtr=currentThread->space->mmapEntries;
+  while(mmapPtr->next){
+    mmapPtr=mmapPtr->next;
+    if(vpn>=mmapPtr->beginPage && vpn<=mmapPtr->endPage)
+      return mmapPtr;
+  }
+  return 0;
+}
+
+//----------------------------------------------------------------------
+// PageOutMmapSpace
+//      Pages out stuff being mmaped (or just between beginPage and
+// endPage.
+//----------------------------------------------------------------------
+
+void PageOutMmapSpace(int beginPage, int endPage)
+{
+  int vpn;
+  int phyPage;
+
+  for(vpn=beginPage; vpn<=endPage; vpn++){
+    if((phyPage=VpnToPhyPage(vpn))==-1)
+      continue;
+    DoPageOut(phyPage);
+  }
+}
