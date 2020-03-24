0de3a14d1c5f9047e49433f1cecac52b95aac4c6
adriangohjw <adriangohjw@gmail.com>
2020-03-24 14:07:56 +0800

    add "Top 5 senders"

--- CZ3006/Lab 4/main.py
+++ CZ3006/Lab 4/main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
 
+# state name of file to use
 FILENAME = 'test_SFLow_data.csv'
 
+# read from CSV file
 matrix = np.genfromtxt(FILENAME, delimiter=',', dtype=None, encoding='utf-8')
 
+# put data into a DataFrame + clean up + add headers
 df = pd.DataFrame(data=matrix)
 df = df.drop(df.columns[len(df.columns)-1], axis=1)
 df.columns = [    
@@ -13,3 +16,9 @@ df.columns = [
     "udp_src_port/tcp_src_port/icmp_type", "udp_dst_port/tcp_dst_port/icmp_code",
     "tcp_flags", "packet_size", "IP_size", "sampling_rate"
 ]
+
+print("TOP 5 senders")
+senders_grouped = df.groupby('src_IP').size().reset_index(name='Count')
+senders_grouped = senders_grouped.sort_values(['Count'], ascending=False)
+top5_senders = senders_grouped.head(5)
+print(top5_senders)
