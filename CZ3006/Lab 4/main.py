ffa83b59b57f8d65c6053f594dc03a8053e4d9b1
adriangohjw <adriangohjw@gmail.com>
2020-04-01 20:26:24 +0800

    improve code output readability

--- CZ3006/Lab 4/main.py
+++ CZ3006/Lab 4/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 # state name of file to use
-FILENAME = 'test_SFLow_data.csv'
+FILENAME = 'SFlow_Data_lab4.csv'
 
 # read from CSV file
 matrix = np.genfromtxt(FILENAME, delimiter=',', dtype=None, encoding='utf-8')
@@ -17,46 +17,54 @@ df.columns = [
     "tcp_flags", "packet_size", "IP_size", "sampling_rate"
 ]
 
-print("TOP 5 senders")
+
+# EXERCISE 4A: TOP TALKERS AND LISTENERS
+print("\nTOP 5 senders")
 senders_grouped = df.groupby('src_IP').size().reset_index(name='Count')
 senders_grouped = senders_grouped.sort_values(['Count'], ascending=False)
 top5_senders = senders_grouped.head(5)
 print(top5_senders)
 
-print("TOP 5 listeners")
+print("\nTOP 5 listeners")
 listeners_grouped = df.groupby('dst_IP').size().reset_index(name='Count')
 listeners_grouped = listeners_grouped.sort_values(['Count'], ascending=False)
 top5_listeners = listeners_grouped.head(5)
 print(top5_listeners)
 
+
 # EXERCISE 4B: TRANSPORT PROTOCOL 
-print("Transport Protocol Percentage breakdown")
+print("\nTransport Protocol Percentage breakdown")
 count_row = df.shape[0]
 df_grouped = df.groupby('IP_protocol').size().reset_index(name='Count')
 df_grouped = df_grouped.sort_values(['Count'], ascending=False)
-df_grouped = df_grouped.loc[df_grouped['IP_protocol'].isin([6, 17])]
 df_grouped['Percentage'] = df_grouped['Count'] / count_row * 100
 top5 = df_grouped.head(5)
 print(top5)
 
+
 # EXERCISE 4C: APPLICATIONS PROTOCOL
-print("Top 5 frequently used application protocol")
+print("\nTop 5 frequently used application protocol")
 df_grouped = df.groupby('udp_dst_port/tcp_dst_port/icmp_code').size().reset_index(name='Count')
 df_grouped = df_grouped.sort_values(['Count'], ascending=False)
-top5 = df_grouped.head(5)
+top5 = df_grouped.head(10)
 print(top5)
 
+
 # EXERCISE 4D: TRAFFIC INTENSITY
-print("Finding traffic intensity of the network")
+print("\nFinding traffic intensity of the network")
 count_row = df.shape[0]
 df_packets = df
-df_packets['Total_packet_size'] = df_packets['packet_size'] * df_packets['sampling_rate']
+
+total_traffic_sampled = df_packets['IP_size'].sum() * 10**-6
+print("Total traffic: {} MB". format(total_traffic_sampled))
+
+df_packets['Total_packet_size'] = df_packets['IP_size'] * 1000 * 10**-6
 total_traffic = df_packets['Total_packet_size'].sum()
-print("Total traffic: {}". format(total_traffic))
+print("Total traffic: {} MB". format(total_traffic))
 
 
 # Most communicated pairs
-print("Finding most communicated pairs in the network")
+print("\nFinding most communicated pairs in the network")
 df_new = df
 df_new['IP_pair'] = None
 
