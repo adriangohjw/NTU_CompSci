7544803d34388c2bbb37a462a4dd6c784bfb9ca2
adriangohjw <adriangohjw@gmail.com>
2020-10-17 02:49:59 +0800

    Implement basic Q-Learning

--- CZ3005/Assignment 2/test.py
+++ CZ3005/Assignment 2/test.py
@@ -3,13 +3,17 @@ import argparse
 import random
 import numpy as np
 from environment import TreasureCube
+from pprint import pprint
 
 
 # you need to implement your agent based on one RL algorithm
 class RandomAgent(object):
     def __init__(self):
         self.action_space = ['left','right','forward','backward','up','down'] # in TreasureCube
-        self.Q = defaultdict ( lambda : np . zeros ( len ( self . action_space ) ) )
+        self.Q = defaultdict(lambda: np.zeros(len(self.action_space)))
+        self.discount = 0.99
+        self.learning_rate = 0.5
+        self.exploration_rate = 0.01
 
     def take_action(self, state):
         action = random.choice(self.action_space)
@@ -18,7 +22,23 @@ class RandomAgent(object):
     # implement your train/update function to update self.V or self.Q
     # you should pass arguments to the train function
     def train(self, state, action, next_state, reward):
-        pass
+        # Q(state, action) = Reward(state, action) + Discount * Max[Q(next state, all actions)]
+        action_index = self.__get_action_index(action)
+        self.Q[state][action_index] = reward + (self.discount * self.__get_max_Q_values(next_state))
+
+    def __get_action_index(self, action):
+        if action not in self.action_space:
+            pass
+        return self.action_space.index(action)
+
+    def __get_max_Q_values(self, state):
+        return max(self.Q[state])
+        
+    def round_up_Q_values(self, num_of_dp):
+        for key, values in self.Q.items():
+            for index in range(len(values)):
+                self.Q[key][index] = round(values[index], num_of_dp)
+
 
 def test_cube(max_episode, max_step):
     env = TreasureCube(max_step=max_step)
@@ -34,13 +54,15 @@ def test_cube(max_episode, max_step):
             reward, terminate, next_state = env.step(action)
             episode_reward += reward
             # you can comment the following two lines, if the output is too much
-            env.render() # comment
-            print(f'step: {t}, action: {action}, reward: {reward}') # comment
+            # env.render() # comment
+            # print(f'step: {t}, action: {action}, reward: {reward}') # comment
             t += 1
             agent.train(state, action, next_state, reward)
             state = next_state
         print(f'epsisode: {epsisode_num}, total_steps: {t} episode reward: {episode_reward}')
 
+    agent.round_up_Q_values(num_of_dp=3)
+    pprint(dict(agent.Q))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test')
