78d6c4821ed0d9e293dec35d52d88d2a9376c2cb
adriangohjw <adriangohjw@gmail.com>
2020-11-18 21:03:20 +0800

    Beautify keyboard options

--- CZ3005/Assignment 4/keyboards.py
+++ CZ3005/Assignment 4/keyboards.py
@@ -1,8 +1,18 @@
-from telepot.namedtuple import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
 def generateKB(options):
   new_list = []
   for item in options:
     new_list.append(item.capitalize().replace("_", " "))
 
-  return ReplyKeyboardMarkup(keyboard=[new_list])
+  n = 2
+  new_segmented_list = [new_list[i * n:(i + 1) * n] for i in range((len(new_list) + n - 1) // n )]
+
+  keyboard_list = []
+  for inner_list in new_segmented_list:
+    temp_list = []
+    for item in inner_list:
+      temp_list.append(KeyboardButton(text=item))
+    keyboard_list.append(temp_list)
+    
+  return ReplyKeyboardMarkup(keyboard=keyboard_list, resize_keyboard=True, one_time_keyboard=True)
